25a9d5aba0bdbb73c00ff07144dce910e5bb7330
Natalia Popova <sverdlova3@gmail.com>
2023-01-12 15:05:35 +0500

    add func done

--- feedback/views.py
+++ feedback/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'feedback/feedback.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        if len(name) == 0:
+            return render(request, 'feedback/feedback.html', context={'got_error': True})
+        print(name)
+        return HttpResponseRedirect('/done')
+    return render(request, 'feedback/feedback.html', context={'got_error': False})
+
+
+def done(request):
+    return render(request, 'feedback/done.html')
+
+
