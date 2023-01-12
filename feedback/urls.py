1d735ada24bd5934b4df31c934949aff58956576
Natalia Popova <sverdlova3@gmail.com>
2023-01-12 15:05:24 +0500

    add path done

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, done
 
 urlpatterns = [
     path('', index),
+    path('done', done)
 ]
