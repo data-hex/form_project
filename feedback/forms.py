6cf69152aeb5aeec38ccd0a22f1c6f003562fd5e
Natalia Popova <sverdlova3@gmail.com>
2023-01-13 15:11:55 +0500

    create class FeedbackForm

---
+++ feedback/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class FeedbackForm(forms.Form):
+    name = forms.CharField()
+    surname = forms.CharField()
+    feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 40}))
