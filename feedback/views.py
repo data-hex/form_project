880e0c7319c72679fc577a37d2ed493a348a11c2
Natalia Popova <sverdlova3@gmail.com>
2023-01-13 15:12:21 +0500

    edit index, change form

--- feedback/views.py
+++ feedback/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from .forms import FeedbackForm
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-        name = request.POST['name']
-        if len(name) == 0:
-            return render(request, 'feedback/feedback.html', context={'got_error': True})
-        print(name)
-        return HttpResponseRedirect('/done')
-    return render(request, 'feedback/feedback.html', context={'got_error': False})
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect('/done')
+    form = FeedbackForm()
+    return render(request, 'feedback/feedback.html', context={'form': form})
 
 
 def done(request):
