396e1d881193f282bc1566947840d183bb106d9f
Natalia Popova <sverdlova3@gmail.com>
2023-01-16 14:24:01 +0500

    validation form

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
 class FeedbackForm(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(label='Имя', max_length=7, min_length=2,
+                           error_messages={'max_length': "Слишком много символов",
+                                           'min_length': "Слишком мало символов",
+                                           'required': "Это обязательное поле"})
     surname = forms.CharField()
     feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 40}))
+    rating = forms.IntegerField(label='Рейтинг', max_value=5, min_value=1)
