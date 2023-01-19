5401a562789402f6f0a4b3d844109973390eb9e1
Natalia Popova <sverdlova3@gmail.com>
2023-01-19 14:59:47 +0500

    ModelForm

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,10 +1,40 @@
 from django import forms
+from .models import Feedback
 
-class FeedbackForm(forms.Form):
-    name = forms.CharField(label='Имя', max_length=7, min_length=2,
-                           error_messages={'max_length': "Слишком много символов",
-                                           'min_length': "Слишком мало символов",
-                                           'required': "Это обязательное поле"})
-    surname = forms.CharField()
-    feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 40}))
-    rating = forms.IntegerField(label='Рейтинг', max_value=5, min_value=1)
+#class FeedbackForm(forms.Form):
+    #name = forms.CharField(label='Имя', max_length=7, min_length=2,
+                           #error_messages={'max_length': "Слишком много символов",
+                         #               'min_length': "Слишком мало символов",
+                            #               'required': "Это обязательное поле"})
+    #surname = forms.CharField()
+    #feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 40}))
+    #rating = forms.IntegerField(label='Рейтинг', max_value=5, min_value=1)
+
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        #fields = ['name', 'rating', 'surname']
+        fields = '__all__'
+        #exclude = ['rating']
+        labels = {
+            'name': 'Имя',
+            'surname': 'Фамилия',
+            'feedback': 'Отзыв',
+            'rating': 'Рейтинг'
+        }
+        error_messages = {
+            'name': {'max_length': 'ой как много символов',
+                     'min_length': 'ой как мало символов',
+                     'required': 'Не должно быть пустым'
+                     },
+            'surname': {'max_length': 'ой как много символов',
+                        'min_length': 'ой как мало символов',
+                        'required': 'Не должно быть пустым'
+                        },
+            'feedback': {'max_length': 'ой как много символов',
+                         'min_length': 'ой как мало символов',
+                         'required': 'Не должно быть пустым'
+                         }
+        }
+
