2ffe00bd2b17ee038bd5235d50aabdb366d75bec
Natalia Popova <sverdlova3@gmail.com>
2023-01-24 14:21:05 +0500

    create update_feedback

--- feedback/views.py
+++ feedback/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
 from .models import Feedback
 
+
 # Create your views here.
 def index(request):
     if request.method == 'POST':
@@ -16,7 +17,18 @@ def index(request):
     return render(request, 'feedback/feedback.html', context={'form': form})
 
 
+def update_feedback(request, id_feedback):
+    feed = Feedback.objects.get(id=id_feedback)
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST, instance=feed)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return HttpResponseRedirect('/done')
+    else:
+        form = FeedbackForm(instance=feed)
+    return render(request, 'feedback/feedback.html', context={'form': form})
+
+
 def done(request):
     return render(request, 'feedback/done.html')
-
-
