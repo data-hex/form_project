9e18ea83de20b1be1f86612dbdb8899a1ed1db6c
Natalia Popova <sverdlova3@gmail.com>
2023-01-24 15:24:40 +0500

    create FeedBackView, DoneView, FeedBackUpdateView

--- feedback/views.py
+++ feedback/views.py
@@ -2,9 +2,42 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
 from .models import Feedback
+from django.views import View
 
 
 # Create your views here.
+class FeedBackView(View):
+    def get(self, request):
+        form = FeedbackForm()
+        return render(request, 'feedback/feedback.html', context={'form': form})
+    def post(self, request):
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return HttpResponseRedirect('/done')
+
+
+class DoneView(View):
+    def get(self, request):
+        return render(request, 'feedback/done.html')
+
+class FeedBackUpdateView(View):
+
+     def get(self,request, id_feedback):
+        feed = Feedback.objects.get(id=id_feedback)
+        form = FeedbackForm(instance=feed)
+        return render(request, 'feedback/feedback.html', context={'form': form})
+     def post(self, request, id_feedback):
+        feed = Feedback.objects.get(id=id_feedback)
+        form = FeedbackForm(request.POST, instance=feed)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return HttpResponseRedirect('/done')
+
+
+'''
 def index(request):
     if request.method == 'POST':
         form = FeedbackForm(request.POST)
@@ -31,4 +64,4 @@ def update_feedback(request, id_feedback):
 
 
 def done(request):
-    return render(request, 'feedback/done.html')
+    return render(request, 'feedback/done.html')'''
