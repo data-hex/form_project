0b315519739edd2040079341981e71f33fb9f879
Natalia Popova <sverdlova3@gmail.com>
2023-01-24 14:20:51 +0500

    path for update_feedback

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, done
+from .views import index, done, update_feedback
 
 urlpatterns = [
     path('', index),
-    path('done', done)
+    path('done', done),
+    path('<int:id_feedback>', update_feedback)
 ]
