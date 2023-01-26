52fa3e942a64e75a262535eee3c588b02128cc4c
Natalia Popova <sverdlova3@gmail.com>
2023-01-26 14:56:02 +0500

    create path list

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import FeedBackView, DoneView, FeedBackUpdateView
+from .views import FeedBackView, DoneView, FeedBackUpdateView, ListFeedBack, DetailFeedBack
 
 urlpatterns = [
     path('', FeedBackView.as_view()),
     path('done', DoneView.as_view()),
-    path('<int:id_feedback>', FeedBackUpdateView.as_view())
+    path('<int:id_feedback>', FeedBackUpdateView.as_view()),
+    path('list', ListFeedBack.as_view()),
+    path('detail/<int:id_feedback>', DetailFeedBack.as_view())
 ]
