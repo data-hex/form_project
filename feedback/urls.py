660892ca88d36d4b893879379e5d37a833597eaf
Natalia Popova <sverdlova3@gmail.com>
2023-02-06 14:10:06 +0500

    add path FeedBackViewUpdate

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import FeedBackView, DoneView, FeedBackUpdateView, ListFeedBack, DetailFeedBack
+from .views import FeedBackView, DoneView, FeedBackUpdateView, ListFeedBack, DetailFeedBack, FeedBackViewUpdate
 
 urlpatterns = [
     path('', FeedBackView.as_view()),
     path('done', DoneView.as_view()),
     path('<int:id_feedback>', FeedBackUpdateView.as_view()),
     path('list', ListFeedBack.as_view()),
-    path('detail/<int:pk>', DetailFeedBack.as_view())
+    path('detail/<int:pk>', DetailFeedBack.as_view()),
+    path('update/<int:pk>', FeedBackViewUpdate.as_view())
 ]
