4e581d68ef6d1b1586d4d1d54655ba73b0f6e7f3
Natalia Popova <sverdlova3@gmail.com>
2023-02-08 13:55:55 +0500

    edit GalleryView

--- gallery/views.py
+++ gallery/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import GalleryUploadForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+def storage_file(file):
+    with open('gallery_tmp/new_image.jpg', 'wb+') as new_file:
+        for chunk in file.chunks():
+            new_file.write(chunk)
+
 class GalleryView(View):
 
     def get(self, request):
-        return render(request, 'gallery/load_file.html')
+        form = GalleryUploadForm()
+        return render(request, 'gallery/load_file.html', {'form': form})
 
     def post(self, request):
-        pass
+        form = GalleryUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            storage_file(form.cleaned_data['image'])
+            return HttpResponseRedirect('load_image')
+        return render(request, 'gallery/load_file.html', {'form': form})
