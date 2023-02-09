77d5cbf39ca55395349e75a3e0136acfb21b9457
Natalia Popova <sverdlova3@gmail.com>
2023-02-09 16:00:31 +0500

    path for ListGallery

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import CreateGalleryView
+from .views import CreateGalleryView, ListGallery
 
 urlpatterns = [
     path('load_image', CreateGalleryView.as_view()),
+    path('list_image', ListGallery.as_view())
 ]
